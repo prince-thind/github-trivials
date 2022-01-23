1e0f9617086af183901466500565aedc68ecbbaf
Prince Thind <Princethind799@gmail.com>
2022-01-23 13:30:50 +0530

    add fetch response file

---
+++ fetchRepos.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const fs = require("fs");
+
+main();
+
+async function main() {
+  const data = await getRepos();
+  const repos = data.map((repo) => ({
+    id: repo.id,
+    name: repo.name,
+  }));
+
+  const res = JSON.stringify(repos, null, 1);
+  fs.writeFileSync("repos.json", res);
+}
+
+async function getRepos() {
+  const response = await fetchResponse();
+  const items = Array.from(response.items);
+  return items;
+}
+
+async function fetchResponse() {
+  const response = await axios.get(process.env.URL, {
+    headers: {
+      authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+  return response.data;
+}
