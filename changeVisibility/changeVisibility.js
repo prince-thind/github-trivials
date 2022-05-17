d9e9a14511f07f510c6b2ced971b9cdc1930598f
Prince Thind <Princethind799@gmail.com>
2022-05-17 18:09:52 +0530

    implement consistent visibility change

--- changeVisibility/changeVisibility.js
+++ changeVisibility/changeVisibility.js
@@ -4,29 +4,35 @@ const fetchResponses = require("./fetchRepos.js");
 const axios = require("axios");
 const fs = require("fs");
 
-const special = [
-
-];
-
-const mode = getMode();
-
 main();
 
 async function main() {
   await fetchResponses();
-  const repos = JSON.parse(fs.readFileSync("./changeVisibility/repos.json"));
+  const mode = getMode();
 
-  for (const { id } of repos) {
-    special.forEach((e) => {
-      if (e.id == id) {
-        changeVisibility(id, e.mode);
-      }
-    });
+  const repos = JSON.parse(fs.readFileSync("./changeVisibility/repos.json"));
+  const specialRepos = JSON.parse(
+    fs.readFileSync("./changeVisibility/specialRepos.json")
+  );
+  const nonSpecialRepos = repos.filter((repo) =>
+    specialRepos.some((r) => r.id != repo.id)
+  );
+
+  for (const { id } of nonSpecialRepos) {
+    changeVisibility(id, mode);
+  }
+
+  for (const { id, mode } of specialRepos) {
     changeVisibility(id, mode);
   }
 }
 
 async function changeVisibility(id, mode) {
+  const repoVisibilityStatus = await getRepoVisbilityStatus(id);
+  const noChangeRequired = repoVisibilityStatus == mode;
+
+  if (noChangeRequired) return;
+
   try {
     await axios.patch(
       process.env.REPO_URL + id,
@@ -48,4 +54,16 @@ function getMode() {
     return mode;
   }
   return "private";
+}
+
+async function getRepoVisbilityStatus(id) {
+  const response = await axios.get(process.env.REPO_URL + id, {
+    headers: {
+      authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  const data = response.data;
+  const visibility = data.visibility;
+  return visibility;
 }
