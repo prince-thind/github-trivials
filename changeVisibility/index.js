fca1eb76e74bba19513f950e404c9c5e274e2c1a
Prince Thind <princethind799@gmail.com>
2022-12-11 20:27:17 +0530

    improve change visbility function

--- changeVisibility/index.js
+++ changeVisibility/index.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 import { Octokit } from "@octokit/rest";
 import getRepos from '../lib/getRepos.js';
-import getUsername from '../lib/getUsername.js';
 import ignoredRepos from './ignored.js'
 
 dotenv.config();
@@ -14,33 +13,38 @@ main();
 
 async function main() {
     const repos = await getRepos(octokit);
-    const username = await getUsername(octokit);
 
     // const testRepos = repos.filter(r => r.name == 'git-test')
     const mode = process.argv[2];
+    const ignoredReposArray = Object.keys(ignoredRepos);
 
-
-    //handle normal repos
     for (const repoObj of repos) {
-        const ignoredReposArray = Object.keys(ignoredRepos)
-        if (ignoredReposArray.includes(repoObj.name)) continue;
+        if (ignoredReposArray.includes(repoObj.name)) {
+            //handle special repos
+            changeVisbility(repoObj.ownerName, repoObj.name, ignoredRepos[repoObj.name].mode)
+        }
 
-        await changeVisbility(username, repoObj.name, mode)
+        //handle normal repos
+        changeVisbility(repoObj.ownerName, repoObj.name, mode)
     }
 
 
-    //handle special repos
-    for (const repoName in ignoredRepos) {
-        await changeVisbility(username, repoName, ignoredRepos[repoName].mode)
-    }
+
 }
 
 async function changeVisbility(owner, repo, mode) {
-    await octokit.rest.repos.update({
-        owner,
-        repo,
-        private: mode === "private" ? true : false,
-    });
+    try {
+        await octokit.rest.repos.update({
+            owner,
+            repo,
+            private: mode === "private" ? true : false,
+        });
+
+    }
+    catch (e) {
+        console.error(e.message);
+        console.log(`skipping ${owner}/${repo}\n`)
+    }
 }
 
 
