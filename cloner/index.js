e8faf0a43ea7bdc40806b3f402c923a63690c2c3
Prince Thind <princethind799@gmail.com>
2022-12-11 19:09:39 +0530

    implement crude cloning algorithm

--- cloner/index.js
+++ cloner/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const { Octokit } = require("@octokit/rest");
+const shell = require('shelljs')
+const path = require('path')
 const getUsername = require('../lib/getUsername');
 
 const octokit = new Octokit({
@@ -13,13 +15,29 @@ main();
 async function main() {
     username = await getUsername(octokit);
     const repos = await getRepos();
-    console.log(repos)
+    // const testRepos = repos.filter(r => r.name == 'git-test');
+    await cloneRepos(repos)
+}
+
+async function cloneRepos(repos) {
+    const absolutePath = path.resolve("./cloner/output");
+    shell.mkdir("-p", absolutePath)
+    shell.cd(absolutePath)
+
+    const repoPromises = [];
+    for (const repo of repos) {
+        const repoPromise = (shell.exec(`git clone ${repo.cloneUrl}`, { async: true }))
+        repoPromises.push(repoPromise)
+    }
+
+    await Promise.allSettled(repoPromises);
+
 }
 
 async function getRepos() {
     const repos = await octokit.rest.repos.listForAuthenticatedUser();
     return repos.data.map(e => {
-        const {name,private,id, html_url,homepage}=e;
-        return {name,private,id,url:html_url,homepage}
+        const { name, private, id, ssh_url, homepage } = e;
+        return { name, private, id, cloneUrl: ssh_url, homepage }
     });
 }
