d165ab1358962c42935d15054dcec99c7221ea7f
Prince Thind <princethind799@gmail.com>
2022-12-11 19:41:01 +0530

    add rate limit for cloning

--- cloner/index.js
+++ cloner/index.js
@@ -1,19 +1,17 @@
-require('dotenv').config();
-const { Octokit } = require("@octokit/rest");
-const shell = require('shelljs')
-const path = require('path')
-const getUsername = require('../lib/getUsername');
+import dotenv from 'dotenv'
+import { Octokit } from "@octokit/rest";
+import shell from 'shelljs'
+import path from 'path'
+import pLimit from 'p-limit'
+dotenv.config();
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 
-let username = null;
-
 main();
 
 async function main() {
-    username = await getUsername(octokit);
     const repos = await getRepos();
     // const testRepos = repos.filter(r => r.name == 'git-test');
     await cloneRepos(repos)
@@ -24,20 +22,34 @@ async function cloneRepos(repos) {
     shell.mkdir("-p", absolutePath)
     shell.cd(absolutePath)
 
+    const limit = pLimit(5)
     const repoPromises = [];
+
     for (const repo of repos) {
-        const repoPromise = (shell.exec(`git clone ${repo.cloneUrl}`, { async: true }))
-        repoPromises.push(repoPromise)
+        const repoPromiseAsyncFunction = () => cloneRepo(repo.cloneUrl)
+        repoPromises.push(limit(repoPromiseAsyncFunction))
     }
 
-    await Promise.allSettled(repoPromises);
+    await Promise.all(repoPromises);
+
+}
+
+async function cloneRepo(url) {
+    //todo error handling
+    return new Promise((resolve, reject) => {
+        shell.exec(`git clone ${url}`, (code) => {
+            resolve(code)
+        })
+    })
 
 }
 
 async function getRepos() {
-    const repos = await octokit.rest.repos.listForAuthenticatedUser();
+    const repos = await octokit.rest.repos.listForAuthenticatedUser({
+        affiliation: 'organization_member,owner', per_page: 100
+    });
     return repos.data.map(e => {
-        const { name, private, id, ssh_url, homepage } = e;
-        return { name, private, id, cloneUrl: ssh_url, homepage }
+        const { name, isPrivate, id, ssh_url, homepage } = e;
+        return { name, isPrivate, id, cloneUrl: ssh_url, homepage }
     });
 }
